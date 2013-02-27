a50433a875c6b5d05dd4ce1a65c45fd87abd7725
Anup Bishnoi <pixelsallover@gmail.com>
2013-02-27 15:59:24 +0530

    changing script urls to custom piped version
    works in localhost. checking on heroku

--- main/pipe/routes.js
+++ main/pipe/routes.js
@@ -1,7 +1,9 @@
 (function () {
     "use strict";
 
-    var request = require("request");
+    var request = require("request"),
+        jsdom = require("jsdom"),
+        _ = require("underscore");
 
     var form = "<form action='/pipe' method='post'><input name='url' type='text'/><input type='submit'/></form>";
 
@@ -12,7 +14,39 @@
         if (url) {
             log("got url: " + url);
             try {
-                request(url).pipe(res);
+                request(url, function (urlErr, urlRes, urlBody) {
+                    jsdom.env(urlBody, ["http://code.jquery.com/jquery.js"], function (err, window) {
+                        var $ = window.$;
+
+                        var scripts = $("script").filter(function (i, elem) {
+                            return elem.src;
+                        });
+
+                        function sendResponse() {
+                            console.log("script length achieved: " + scripts.length);
+                            res.send($("<div>").append($("html").clone()).remove().html());
+                        }
+
+                        var sendResponseWhenDone = _.after(scripts.length, sendResponse);
+                        function newScriptSrc(scriptSrc, scriptSrcCallback) {
+                            request(
+                                "http://tinyurl.com/api-create.php?url=" + scriptSrc,
+                                function (tinyErr, tinyRes, tinyBody) {
+                                    var newUrl = "http://www.roughregister.com/pipe?url=" + tinyBody;
+                                    scriptSrcCallback(newUrl);
+                                    console.log("tinyUrl changed " + scriptSrc + " to " + newUrl);
+                                }
+                            );
+                        }
+
+                        scripts.each(function (index, elem) {
+                            newScriptSrc(elem.src, function (src) {
+                                elem.src = src;
+                                sendResponseWhenDone();
+                            });
+                        });
+                    });
+                });
             } catch (e) {
                 log("error piping url: " + url);
                 res.send(e.toString() + "<br>" + form);
