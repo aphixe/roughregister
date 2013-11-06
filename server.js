2bc182b6bfccc34b517d501b06d44a1bf70d2518
Anup Bishnoi <pixelsallover@gmail.com>
2013-11-07 01:12:39 +0530

    Added sockets initialization to server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var app = require("apper")(__dirname);
 
-if (app.init()) {
+if (app.init({ sockets: true })) {
     app.start(process.env.PORT || 8000);
 }
 
 process.on("uncaughtException", function (error) {
     console.log(error.stack);
 });
+
