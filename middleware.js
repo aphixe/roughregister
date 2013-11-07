b18872a0a08dfe7dd6a91b715b5c12fb233b78a6
Anup Bishnoi <pixelsallover@gmail.com>
2013-11-08 00:15:01 +0530

    awesome, namespaced sockets work perfectly

--- middleware.js
+++ middleware.js
@@ -4,10 +4,9 @@ module.exports = function (app, mountPath) {
 
     log("Testing socket.io support");
 
-    io.sockets.on("connection", function (socket) {
+    io.on("connection", function (socket) {
         setInterval(function () {
             socket.emit("from root");
         }, 2000);
     });
-    io.sockets.on("connection", function () { console.log("lo bhai multiple"); });
 };
--- socket/middleware.js
+++ socket/middleware.js
@@ -2,9 +2,9 @@ module.exports = function (app, mountPath) {
     var io = app.socketIO,
         log = app.log;
 
-    log("Testing socket.io support");
+    log("Testing socket.io support.");
 
-    io.sockets.on("connection", function (socket) {
+    io.on("connection", function (socket) {
         console.log("yay");
         setInterval(function () {
             socket.emit("from socket");
