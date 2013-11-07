ffe0c3269f79b4d2902c8cbb3ac8e19fd9353631
Anup Bishnoi <pixelsallover@gmail.com>
2013-11-08 01:29:23 +0530

    removed extraneous socket experiments and started with drone demo
    yo!

--- drone/middleware.js
+++ drone/middleware.js
@@ -2,11 +2,7 @@ module.exports = function (app, mountPath) {
     var io = app.socketIO,
         log = app.log;
 
-    log("Testing socket.io support");
-
     io.on("connection", function (socket) {
-        setInterval(function () {
-            socket.emit("from root");
-        }, 2000);
+        socket.emit("connected");
     });
 };
