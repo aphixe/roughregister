2b3ca4ae2be7b71cb99de8c2fe2f787cb364e59d
Anup Bishnoi <pixelsallover@gmail.com>
2013-11-07 21:45:27 +0530

    back to trying out apper's socket.io support

--- server.js
+++ server.js
@@ -1,28 +1,9 @@
 var app = require("apper")(__dirname);
 
-if (app.init()) {
-    //app.start(process.env.PORT || 3000);
+if (app.init({ sockets: true })) {
+    app.start(process.env.PORT || 3000);
 }
 
 process.on("uncaughtException", function (error) {
     console.log(error.stack);
 });
-
-var sockets = require("socket.io");
-
-var server = require("http").createServer(app.expressApp),
-    io = sockets.listen(server);
-
-io.configure(function () {
-    io.set("transports", ["xhr-polling"]);
-    io.set("polling duration", 10);
-});
-
-io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
-    });
-});
-
-server.listen(process.env.PORT || 3000);
