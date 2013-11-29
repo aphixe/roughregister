6d68681fbd27b10c34c7b1c03eaf90752ad7d16f
Anup Bishnoi <pixelsallover@gmail.com>
2013-11-29 11:35:59 +0530

    added the remaining built-in animations

--- drone/command-defaults.js
+++ drone/command-defaults.js
@@ -3,7 +3,9 @@ module.exports = {
 	    flipLeft: [100],
 	    flipRight: [100],
 	    wave: [6000],
-	    doublePhiThetaMixed: [8000]
+	    doublePhiThetaMixed: [8000],
+	    turnaroundGodown: [4000],
+	    phiDance: [8000]
 	},
 	up: [0.2],
 	down: [0.2],
